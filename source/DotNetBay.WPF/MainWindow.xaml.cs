482cd6f8c612d970b13d12d8685b6455e03e7c87
Cyril Halmo <cyril.halmo@students.fhnw.ch>
2015-09-26 14:14:07 +0200

    First part of lab 2.

---
+++ source/DotNetBay.WPF/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DotNetBay.Core;
+using DotNetBay.Model;
+
+namespace DotNetBay.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private readonly ObservableCollection<Auction> auctions = new ObservableCollection<Auction>();
+
+        public ObservableCollection<Auction> Auctions
+        {
+            get { return this.auctions; }
+        }
+
+        public MainWindow()
+        {
+            if ((Application.Current as App) != null)
+            {
+                var service = new AuctionService(((App)Application.Current).MainRepository, new SimpleMemberService(((App)Application.Current).MainRepository));
+                foreach (var auction in service.GetAll())
+                {
+                    this.Auctions.Add(auction);
+                }
+            }
+            this.DataContext = this;
+            this.InitializeComponent();
+        }
+
+        public void OpenSellView(object sender, RoutedEventArgs e)
+        {
+            var sellView = new SellView();
+            sellView.ShowDialog();
+        }
+
+        public void PlaceBid(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+    }
+
+    public class BooleanToStatusConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? "Valid" : "Closed";
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ source/DotNetBay.WPF/SellView.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using DotNetBay.Core;
+using DotNetBay.Model;
+
+namespace DotNetBay.WPF
+{
+    /// <summary>
+    /// Interaction logic for SellView.xaml
+    /// </summary>
+    public partial class SellView : Window
+    {
+        public SellView()
+        {
+            this.InitializeComponent();
+        }
+
+        public void AddAuction(object sender, RoutedEventArgs e)
+        {
+            var repo = ((App) Application.Current).MainRepository;
+            var memberService = new SimpleMemberService(repo);
+            var service = new AuctionService(repo, memberService);
+
+            var title = this.Title.Text;
+            var startdate = DateTime.Parse(this.Start.Text);
+            var enddate = DateTime.Parse(this.End.Text);
+            var price = double.Parse(this.StartPrice.Text);
+            var member = memberService.GetCurrentMember();
+
+            var auction = new Auction
+            {
+                Title = title,
+                StartDateTimeUtc = startdate,
+                EndDateTimeUtc = enddate,
+                StartPrice = price,
+                Seller = member
+            };
+
+            service.Save(auction);
+            this.Cancel(sender, e);
+        }
+
+        public void Cancel(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
